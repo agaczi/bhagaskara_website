73fd030dd015cb103d6d22e765843aeeb4196cf5
Agata Maciejewska <aamaciejewska@gmail.com>
2015-12-28 23:02:09 +0100

    slick menu upgrade / final customizing, last update on testing version

--- js/app.js
+++ js/app.js
@@ -100,7 +100,7 @@ $(document).ready(function() {
             dots: true,
             infinite: true,
             slidesToScroll: 1,
-            autoplay: true
+            autoplay: false
         })
 
     }
